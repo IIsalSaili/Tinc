164e9935d60da29309db8787cb8af93a50847f2c
Ilias <98957273+IIsalSaili@users.noreply.github.com>
2022-02-14 15:38:23 +0100

    Add files via upload
    no comment

--- main.ml
+++ main.ml
@@ -1,31 +1,52 @@
 open Random
 
 type joueur = {
-    mutable pv : int;
-    mutable pv_max : int;
+    mutable hp : int;
+    mutable hp_max : int;
     mutable score : int;
-    mutable max_life : int;
-    mutable life : int;
+    mutable score_streak : float;
 }
 
 type tiles = {
-    mutable type : char;
+    mutable typ : char;
     mutable position : int;
 }
 
-type map = {
-    mutable size : int;
-}
+let map = [0,0,0,0,0]
 
-let affiche_map map = 
-     
+let create_player hp = 
+    {hp = hp; hp_max=hp; score = 0; score_streak = 0}
+
+(*let read_next =*) 
+
+    (*Ici faudra mettre toute la partie du code qui lit la prochaine ligne et 
+    modifie la liste map *)
+
+let lose =
+    Printf.sprintf "Et c'est perdu !"
+
+let win =
+    Printf.sprintf "Et c'est gagné !"
+
+let hit = 
+    (*La fonction qui agit quand le joueur marque un point, oui c'était assez évident *)
+    joueur.score <- joueur.score + 100;
+    if joueur.hp < 500 then joueur.hp <- joueur.hp + 50 else joueur.hp <- joueur.hp + 3
+    read_next;
+
+let fail = 
+    (*Soit le joueur a encore des hp et il en perd, soit il a perdu*)
+    if joueur.hp>80 then joueur.hp <- joueur.hp-80 else lose;
 
 
-let hit =
-    joueur
+(*let verif_tile map= 
 
-let verif_tile = 
+(*Pas codable tant que j'ai pas tout le reste, mais bon 
+en gros ça compare le nom de la tile en question à la touche et ça agit en conséquence*)
+
     let touche = wait_next_event [Key_pressed] in
-    if tile.type = touche then hit
+    if tile.type = touche then hit else fail*)
 
 (*let loop = *)
+
+(*Le loop de base pas encore créé*)
